24ccbe06db4dd60a93f85e418032c459064870d6
YtalloXD <r.ytalofelix11@gmail.com>
2025-10-21 13:29:07 -0300

    Criado a pasta UsersCR (Controller, Routes) porém ainda necessita de testes

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,18 @@
-
-const express =require('express');
+const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const PORT = 3000;
 
 app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Servidor funcionando!');
+app.get("/", (req, res) => {
+  res.send("Servidor funcionando!");
 });
+
+const usersRoutes = require("./UsersCR/usersRoutes");
+app.use("/users", usersRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
